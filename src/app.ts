d837ec73e2dc548df017fa8391a3b3e0f13cc545
chryzcode <alabaolanrewaju13@gmail.com>
2024-03-30 10:29:07 +0100

    push

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import "dotenv/config"
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 
+const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT || 8000
-
-const app = express()
-
+const app = express();
 
 const start = async () => {
-    
+  try {
     app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-    })
-}
+      console.log(`server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-
-
+start();
