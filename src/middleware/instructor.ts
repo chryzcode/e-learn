318108d4929afa9de1d339dda489fb59902e5456
chryzcode <alabaolanrewaju13@gmail.com>
2024-03-31 06:37:11 +0100

    push

---
+++ src/middleware/instructor.ts
@@ -0,0 +1,12 @@
+import { User } from "../models/user";
+import { UnauthenticatedError } from "../errors/index";
+
+export default async (req: any, res: any, next: any) => {
+  const { userId } = req.user;
+  const user: any = User.findOne({ _id: userId, userType: "Instructor" });
+  if (!user) {
+    throw new UnauthenticatedError("Authentication as an instructor invalid");
+  }
+  req.user = { userId: user.userId, firstName: user.fullName };
+  next();
+};
