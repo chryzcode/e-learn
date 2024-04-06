1eba8d5c5c521a0dcb2f37c04de5f2e6cfc0d8af
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-06 09:59:20 +0100

    push

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -1,6 +1,21 @@
-import { Payment } from "../models/payment"
+import { Payment } from "../models/payment";
+import { BadRequestError, UnauthenticatedError, NotFoundError } from "../errors/index";
+import { StatusCodes } from "http-status-codes";
+import { courseStudent } from "../models/course";
 
 export const paymentSuccessful = async (req: any, res: any) => {
-    const { paymentId } = req.params
-    const payment = await Payment
-}
+  const { paymentId } = req.params;
+  const payment = await Payment.findOneAndUpdate(
+    { _id: paymentId },
+    { paid: true },
+    { new: true, runValidators: true }
+  );
+  if (!payment) {
+    throw new NotFoundError(`Payment with id ${paymentId} does not exists`);
+  }
+  await courseStudent.create({
+    student: payment.student,
+    course: payment.course,
+  });
+  res.status(StatusCodes.OK).json({ payment });
+};
