bc5a5bce004030a46b9f923bfa24171ac5c223b5
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-06 10:42:19 +0100

    push

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -4,6 +4,6 @@ import { paymentSuccessful, paymentCancelled } from "../controllers/payment";
 const router = express.Router();
 
 router.route("/:paymentId/success").post(paymentSuccessful);
-router.route("/:paymentId/cancel").post(paymentCancelled);
+router.route("/:paymentId/cancel").get(paymentCancelled);
 
 export default router;
