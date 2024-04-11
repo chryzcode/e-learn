d69ba276b48ef1de06f7339ea920cbb64bda8811
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-11 08:51:12 +0100

    push

---
+++ src/models/chatRoom.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const courseRoomSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
+});
+
+const roomMessageSchema = new mongoose.Schema(
+  {
+    message: {
+      type: String,
+      required: [true, "message field is required"],
+    },
+    media: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const courseRoom = mongoose.model("courseRoom", courseRoomSchema);
+export const roomMessage = mongoose.model("roomMessage", roomMessageSchema);
