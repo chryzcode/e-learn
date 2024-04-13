d04ef0a4a6f411521d8958e34c30a814d66e00d3
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-13 12:32:24 +0100

    push

--- src/routes/chatRoom.ts
+++ src/routes/chatRoom.ts
@@ -1,12 +1,22 @@
 import express from "express";
-import { userRooms, roomMessages, sendMessage, editMessage, deleteMessage, exitRoom } from "../controllers/chatRoom";
+import {
+  userRooms,
+  roomMessages,
+  sendMessage,
+  editMessage,
+  deleteMessage,
+  exitRoom,
+  inviteUserToRoom,
+} from "../controllers/chatRoom";
 import authenticateUser from "../middleware/authentication";
+import authenticateStudent from "../middleware/student";
 
 const router = express.Router();
 
 router.route("/").get(authenticateUser, userRooms);
-router.route("/roomId").get(authenticateUser, roomMessages).post(authenticateUser, sendMessage);
-router.route("/roomId/messageId").put(authenticateUser, editMessage).delete(authenticateUser, deleteMessage);
-router.route("/roomId/exit").post(authenticateUser, exitRoom);
+router.route("/:roomId").get(authenticateUser, roomMessages).post(authenticateUser, sendMessage);
+router.route("/:roomId/:messageId").put(authenticateUser, editMessage).delete(authenticateUser, deleteMessage);
+router.route("/:roomId/exit").post(authenticateUser, exitRoom);
+router.route("/:roomId/invite").post(authenticateUser, authenticateStudent, inviteUserToRoom);
 
 export default router;
