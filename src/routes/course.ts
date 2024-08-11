3b65b517e7aea239770ced10dbb4434e9514af29
chryzcode <alabaolanrewaju13@gmail.com>
2024-08-11 16:29:36 +0100

    push

--- src/routes/course.ts
+++ src/routes/course.ts
@@ -28,7 +28,18 @@ import { multerUpload } from "../utils/cloudinaryConfig";
 
 const router = express.Router();
 
-router.route("/").post(authenticateUser, authenticateInstructor, multerUpload.single("video"), createCourse).get(allCourses);
+router
+  .route("/")
+  .post(
+    authenticateUser,
+    authenticateInstructor,
+    multerUpload.fields([
+      { name: "video", maxCount: 1 },
+      { name: "thumbnail", maxCount: 1 },
+    ]),
+    createCourse
+  )
+  .get(allCourses);
 router.route("/instructor/:instructorId").get(getAnInstructorCourses);
 router.route("/my-courses").get(authenticateUser, authenticateInstructor, instructorCourses);
 router.route("/categories").get(courseCategories);
@@ -36,7 +47,9 @@ router.route("/categories/:categoryId").get(filterCourseByCategory);
 router.route("/free").get(freeCourses);
 router.route("/enroll/:courseId").post(authenticateUser, enrollForCourse);
 router.route("/detail/:courseId").get(authenticateUser, courseDetail);
-router.route("/edit/:courseId").put(authenticateUser, authenticateInstructor, editCourse);
+router
+  .route("/edit/:courseId")
+  .put(authenticateUser, authenticateInstructor, multerUpload.fields([{ name: "thumbnail", maxCount: 1 }]), editCourse);
 router.route("/delete/:courseId").delete(authenticateUser, authenticateInstructor, deleteCourse);
 router.route("/like/:courseId").post(authenticateUser, authenticateStudent, likeCourse);
 router.route("/rate/:courseId").post(authenticateUser, authenticateStudent, rateCourse).get(courseRatings);
